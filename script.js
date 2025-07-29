80fe98f1342a6686b22d09f28d112ddb50a22eb8
Rafael Saito <107894019+rafaelsaito@users.noreply.github.com>
2025-07-29 14:20:57 -0300

    lista parte 2

--- script.js
+++ script.js
@@ -7,12 +7,21 @@ function inserirNome () {
 
 inserirNome();
 
-let linguagens = ["JavaScript", "Python", "C"];
-console.log(linguagens[0]) // “JavaScript”
-console.log(linguagens[1]) // “Python”
-console.log(linguagens[2]) // “C”
+let aluno1 = {
+    nome: "Rafael",
+    idade: 29,
+    anoLetivo: "Universidade",
+    materiasFavoritas; ["Matmática", "Programação", "Física"]
+}
 
-const item = document.querySelector("#lista");
-item.textContent = linguagens[0];
-item.textContent = linguagens[1];
-item.textContent = linguagens[2];
+const objeto = document.querySelector("#aluno");
+objeto.textContent = aluno1.nome;
+objeto.textContent = aluno1.idade;
+objeto.textContent = aluno1.materiasFavoritas[2];
+
+
+console.log(aluno1.nome); // "Rafael"
+console.log(aluno1.idade); // 29
+console.log(aluno1.materiasFavoritas);
+// ["Matemática","Programação","Física"]
+console.log(aluno1.materiasFavoritas[2]); // "Física"
